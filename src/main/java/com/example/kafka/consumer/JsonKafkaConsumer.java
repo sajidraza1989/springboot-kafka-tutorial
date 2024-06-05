b54451978630a037b0884e6148a09bd5ca71c1bd
unknown <sajidraza1989@gmail.com>
2024-06-05 17:05:07 +0530

    Consumer usingHeader and Payload with manual ack

--- src/main/java/com/example/kafka/consumer/JsonKafkaConsumer.java
+++ src/main/java/com/example/kafka/consumer/JsonKafkaConsumer.java
@@ -3,16 +3,41 @@ package com.example.kafka.consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-
-import com.example.kafka.model.User;
 
 @Service
 public class JsonKafkaConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
 
+    /*
     @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(User user){
         LOGGER.info(String.format("Json message recieved -> %s", user.toString()));
+    }
+    */
+    
+    /**
+     * Acknowledgment ack -> only work once you have configured the Ack mode as Manual (spring.kafka.listener.ack-mode: MANUAL_IMMEDIATE) 
+     * @param payload
+     * @param offset
+     * @param topic
+     * @param partitionId
+     * @param ack
+     */
+    
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(
+    		@Payload String payload,
+    		@Header(KafkaHeaders.OFFSET) String offset,
+    		@Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+    		@Header(KafkaHeaders.RECEIVED_PARTITION_ID) String partitionId,
+    		Acknowledgment ack
+    		){
+        LOGGER.info(String.format("Json message recieved -> %s", payload.toString()));
+        ack.acknowledge();
     }
 }
--- src/main/java/com/example/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/example/kafka/consumer/KafkaConsumer.java
@@ -4,14 +4,42 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
+   /* 
     @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(String message){
         LOGGER.info(String.format("Message received -> %s", message));
+    }
+    
+    */
+    
+    /**
+     * Acknowledgment ack -> only work once you have configured the Ack mode as Manual (spring.kafka.listener.ack-mode: MANUAL_IMMEDIATE)
+     * @param payload
+     * @param offset
+     * @param topic
+     * @param partitionId
+     * @param ack
+     */
+    
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(
+    		@Payload String payload,
+    		@Header(KafkaHeaders.OFFSET) String offset,
+    		@Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+    		@Header(KafkaHeaders.RECEIVED_PARTITION_ID) String partitionId,
+    		Acknowledgment ack
+    		){
+        LOGGER.info(String.format("Json message recieved -> %s", payload.toString()));
+        ack.acknowledge();
     }
 }
